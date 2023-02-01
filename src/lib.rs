4c59d1908d78d6b65c6d179c66dd5c0ed6496f33
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-02-01 14:37:38 +0000

    week 2 changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,10 @@
-// read in h2h data
-// read in model training data 
-// train model 
-// dataframe of predictions? 
+extern crate polars;
+use polars::error::PolarsResult;
+use polars::frame::DataFrame;
+use polars::io::prelude::CsvReader;
+use polars::prelude::SerReader;
 
-// function to find h2h 
-// function to find results 
-
-pub fn read_csv(filename: &str) -> Vec<f64> {
-    let mut rdr = csv::Reader::from_path(filename).unwrap();
-    let mut data: Vec<f64> = Vec::new();
-    for result in rdr.records() {
-        let record = result.unwrap();
-        let value: f64 = record[0].parse().unwrap();
-        data.push(value);
-    }
-    data
+pub fn read_csv(filename: &str) -> PolarsResult<DataFrame> {
+    let file = filename;
+    CsvReader::from_path(file)?.finish()
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate clap;
 use clap::Parser;
+extern crate polars;
+// use polars::prelude::*;
 
 #[derive(Parser)]
 //add extended help
@@ -33,15 +35,28 @@ enum Commands {
 
 fn main() {
     let cli = Cli::parse();
-    let df = cli_proj::read_csv("final_df.csv");
+    // let train_df = cli_proj::read_csv("final_df.csv").expect("Could not read csv");
+    let h2h_df = cli_proj::read_csv("h2h.csv").expect("Could not read csv");
 
     match cli.command {
         Some(Commands::Stats { player, year }) => {
             println!("Test {0}, {1}", player, year);
         }
         Some(Commands::H2H { player, opponent }) => {
-            println!("{}", opponent);
-            println!("{}", player);
+            let first = if player < opponent {
+                player.clone()
+            } else {
+                opponent.clone()
+            };
+
+            let second = if player > opponent { player } else { opponent };
+
+            // let h2h_df = h2h_df.filter(&col("player1") == lit(first) & (&col("player2") == lit(second))).unwrap();
+
+            println!("First player is {}", first);
+            println!("Second player is {}", second);
+
+            println!("{}", h2h_df.head(Some(1)));
         }
         Some(Commands::Predict { player, opponent }) => {
             println!("{}", opponent);
