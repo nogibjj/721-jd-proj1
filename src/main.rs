a2f010c83e0b57bb60e7b60731a6bdffa45f0642
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-02-07 01:57:08 +0000

    filter dataframe by inputs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate clap;
 use clap::Parser;
 extern crate polars;
-// use polars::prelude::*;
+use polars::lazy::dsl::*;
+use polars::prelude::*;
 
 #[derive(Parser)]
 //add extended help
@@ -25,24 +26,34 @@ enum Commands {
         #[clap(long)]
         opponent: String,
     },
-    Predict {
-        #[clap(long)]
-        player: String,
-        #[clap(long)]
-        opponent: String,
-    },
 }
 
 fn main() {
     let cli = Cli::parse();
-    // let train_df = cli_proj::read_csv("final_df.csv").expect("Could not read csv");
-    let h2h_df = cli_proj::read_csv("h2h.csv").expect("Could not read csv");
-
+    
     match cli.command {
         Some(Commands::Stats { player, year }) => {
+            // read csv
+            let df = cli_proj::read_csv("final_df.csv").expect("Could not read csv");
+            // filter by inputted player and year
+            let player_df = df
+                .lazy()
+                .filter(col("player").eq(lit(player.clone())))
+                .filter(col("year").eq(lit(year as u32)))
+                .collect()
+                .unwrap();
+
+            
+            // find some average stats
+
+            // print accordingly
             println!("Test {0}, {1}", player, year);
+            println!("{}", player_df.head(Some(1)));
         }
         Some(Commands::H2H { player, opponent }) => {
+            // read csv
+            let df = cli_proj::read_csv("h2h.csv").expect("Could not read csv");
+            // determine order of players
             let first = if player < opponent {
                 player.clone()
             } else {
@@ -51,18 +62,22 @@ fn main() {
 
             let second = if player > opponent { player } else { opponent };
 
-            // let h2h_df = h2h_df.filter(&col("player1") == lit(first) & (&col("player2") == lit(second))).unwrap();
+            // filter by player1 and player2 
+            let h2h_df = df
+                .lazy()
+                .filter(col("player1").eq(lit(first.clone())))
+                .filter(col("player2").eq(lit(second.clone())))
+                .collect()
+                .unwrap();
 
+            // find aggregate stats
+
+            // print accordingly
             println!("First player is {}", first);
             println!("Second player is {}", second);
-
+            println!();
             println!("{}", h2h_df.head(Some(1)));
         }
-        Some(Commands::Predict { player, opponent }) => {
-            println!("{}", opponent);
-            println!("{}", player);
-        }
-
         None => {
             println!("No command given");
         }
