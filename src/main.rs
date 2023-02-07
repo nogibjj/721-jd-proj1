553f546d91332447681c8b2648f55b33489c37f2
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-02-07 19:38:50 +0000

    add changes for h2h

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ enum Commands {
 
 fn main() {
     let cli = Cli::parse();
-    
+
     match cli.command {
         Some(Commands::Stats { player, year }) => {
             // read csv
@@ -43,7 +43,6 @@ fn main() {
                 .collect()
                 .unwrap();
 
-            
             // find some average stats
 
             // print accordingly
@@ -62,7 +61,7 @@ fn main() {
 
             let second = if player > opponent { player } else { opponent };
 
-            // filter by player1 and player2 
+            // filter by player1 and player2
             let h2h_df = df
                 .lazy()
                 .filter(col("player1").eq(lit(first.clone())))
@@ -70,13 +69,70 @@ fn main() {
                 .collect()
                 .unwrap();
 
+            // check if h2h_df is empty
+            if h2h_df.height() == 0 {
+                println!("No head-to-head record found for {} and {}", first, second);
+                return;
+            }
+
             // find aggregate stats
+            let agg_rec = h2h_df
+                .clone()
+                .lazy()
+                .groupby([col("player1")])
+                .agg(vec![col("h2h_win").sum(), col("h2h_lose").sum()])
+                .collect()
+                .unwrap();
+
+            let year_agg_rec = h2h_df.clone()
+                .lazy()
+                .groupby([col("year")])
+                .agg(vec![col("h2h_win").sum(), col("h2h_lose").sum()])
+                .collect()
+                .unwrap();
+            //fix column names
+
+            // rename columns in year_agg_rec from h2h_win to win and h2h_lose to lose
+            let year_agg_rec_fin = year_agg_rec.clone()
+                .lazy()
+                .select(&[
+                    col("year").alias("Year"),
+                    col("h2h_win").alias(&first),
+                    col("h2h_lose").alias(&second),
+                ])
+                .collect()
+                .unwrap();
+
+
+            // get value from agg_rec as a scalar value
+            let first_win = agg_rec["h2h_win"].i64().unwrap().get(0).unwrap();
+            let second_win = agg_rec["h2h_lose"].i64().unwrap().get(0).unwrap();
+            // find min and max year from year agg rec
+            let duration = year_agg_rec["year"].i64().unwrap().max().unwrap()
+                - year_agg_rec["year"].i64().unwrap().min().unwrap();
+            let total = first_win + second_win;
 
             // print accordingly
-            println!("First player is {}", first);
-            println!("Second player is {}", second);
+            println!(
+                "{} and {} have played {} times over the course of {} years",
+                first,
+                second,
+                total,
+                duration
+            );
+            println!(
+                "{} {} ({}%) vs {} ({}%) {}",
+                first,
+                first_win,
+                ((first_win as f64 / total as f64) * 100.0).round(),
+                second_win,
+                ((second_win as f64 / total as f64) * 100.0).round(),
+                second
+            );
+
             println!();
-            println!("{}", h2h_df.head(Some(1)));
+            println!("Stats by Year:");
+            println!("{:?}", year_agg_rec_fin);
         }
         None => {
             println!("No command given");
